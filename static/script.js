378c2cdebc7f6fe10660fa922a764c50ac1d7422
vedant-pati1 <vedantpatil1709@gmail.com>
2023-11-07 19:22:22 +0530

    clear script.js file

--- static/script.js
+++ static/script.js
@@ -1,28 +1,2 @@
 
-let checkboxes = document.querySelectorAll(".checkbox");
-let tasks = document.querySelectorAll(".task");
-
-let submitBtn = document.createElement('input');
-// let submitBtn = document.querySelector('.AddBtn');
-let a = document.querySelector("s");
-
-
-submitBtn.addEventListener('click',function(){
-
-    console.log("hi!");
-    checkboxes.forEach((checkbox)=>{
-        checkbox.addEventListener('click',function(e){
-            if(this.checked){
-                let num = this.classList[1];
-                tasks.forEach((element)=>{
-                    if(element.classList.contains(`${num}`)){
-                        element.setAttribute('class',"scratch-out");
-                    }
-                })
-            }
-        })
-    })
-
-
-});
   
