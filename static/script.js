fa7481815a23b780990ac697b4dc5a0b74911ce3
vedant-pati1 <vedantpatil1709@gmail.com>
2023-11-11 02:07:34 +0530

    complete logic on client-side

--- static/script.js
+++ static/script.js
@@ -1,2 +1,21 @@
 
-  
+const btn = document.querySelector(".add");
+const input = document.querySelector(".inputbox");
+btn.addEventListener('click', ()=>{
+    let val = input.value;
+    let task = createTask(val);
+    document.body.append(task);
+    input.value = "";
+});
+
+function createTask(val){
+    let div = document.createElement("div");
+    let input = document.createElement('input');
+    let text = document.createElement('p');
+
+    input.setAttribute("type",'checkbox');
+    text.innerText = val
+    div.classList.add('task')
+    div.append(input,text);
+    return div;
+}
